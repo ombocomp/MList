fad3f439671bec586fcaa4e612246c44370650cf
hans <janos.tapolczai@gmail.com>
2014-02-17 00:13:30 +0100

    Implemented more functions on MLists.
    Wrote
    
    * mapAccumML.
    * iterateML, repeatML, replicateML, cycleML.
    * elemML, notElemML.
    * zip[2-7]ML, zipWith[2-7]ML.

--- Data/MList.hs
+++ Data/MList.hs
@@ -61,13 +61,13 @@ module Data.MList (
   unfoldML,
 
   -- * Accumulating maps
-  --mapAccumML,
+  mapAccumML,
 
   -- * Infinite MLists
-  --iterateML,
-  --repeatML,
-  --replicateML,
-  --cycleML,
+  iterateML,
+  repeatML,
+  replicateML,
+  cycleML,
 
   -- * Sublists
   takeML,
@@ -76,23 +76,23 @@ module Data.MList (
   dropWhileML,
 
   -- * Searching MLists
-  --elemML,
-  --notElemML,
+  elemML,
+  notElemML,
 
   -- * Zipping MLists
-  --zipML,
-  --zip3ML,
-  --zip4ML,
-  --zip5ML,
-  --zip6ML,
-  --zip7ML,
+  zipML,
+  zip3ML,
+  zip4ML,
+  zip5ML,
+  zip6ML,
+  zip7ML,
 
-  --zipWithML,
-  --zipWith3ML,
-  --zipWith4ML,
-  --zipWith5ML,
-  --zipWith6ML,
-  --zipWith7ML,
+  zipWithML,
+  zipWith3ML,
+  zipWith4ML,
+  zipWith5ML,
+  zipWith6ML,
+  zipWith7ML,
 
   --unzipML,
   --unzip3ML,
@@ -222,3 +222,204 @@ reverseML = reverse' MNil
   where reverse' acc MNil = return acc
         reverse' acc (x :# xs) = xs >>= reverse' (x :# return acc)
 
+-- |Combines 'foldML' and 'mapML', both applying a function to
+--  every element and accumulating a value.
+mapAccumML :: Monad m
+           => (acc -> a -> m (acc, b))
+           -> acc
+           -> MList m a
+           -> m (acc, MList m b)
+mapAccumML _ acc MNil = return (acc, MNil)
+mapAccumML f acc (x :# xs) = do (acc', y) <- f acc x
+                                (acc'', ys) <- xs >>= mapAccumML f acc'
+                                return (acc'', y :# return ys)
+
+-- |Constructs the infinite MList @[x, f x, f (f x), ...]@.
+iterateML :: Monad m => (a -> m a) -> a -> MList m a
+iterateML f x = x :# liftM (iterateML f) (f x)
+
+-- |Repeats the same element infinitely.
+repeatML :: Monad m => m a -> m (MList m a)
+repeatML x = do x' <- x
+                return $ x' :# repeatML x
+
+-- |Creates an MList consisting of n copies of an element.
+replicateML :: Monad m => Int -> m a -> m (MList m a)
+replicateML n x | n <= 0 = return MNil
+                | n > 0  = do x' <- x
+                              return (x' :# replicateML (n-1) x) 
+
+-- |Repeats an MList infinitely.
+cycleML :: Monad m => MList m a -> MList m a
+cycleML MNil = error "cycleML: tail of empty MList!"
+cycleML xs = xs +++ cycleML xs
+
+-- |Returns True if an MList contains a given element.
+--  If the list is infinite, this is only a semi-decision procedure.
+elemML :: (Monad m, Eq a) => a -> MList m a -> m Bool
+elemML _ MNil = return False
+elemML x (y :# ys) | x == y    = return True
+                   | otherwise = ys >>= elemML y
+
+-- |The negation of 'elemML'.
+notElemML :: (Monad m, Eq a) => a -> MList m a -> m Bool
+notElemML x ys = liftM not (elemML x ys)
+
+-- |Zips two MLists.
+zipML :: Monad m
+      => MList m a
+      -> MList m b
+      -> MList m (a,b)
+zipML = zipWithML (,)
+
+-- |Zips three MLists.
+zip3ML :: Monad m
+       => MList m a
+       -> MList m b
+       -> MList m c
+       -> MList m (a,b,c)
+zip3ML = zipWith3ML (,,)
+
+-- |Zips four MLists.
+zip4ML :: Monad m
+       => MList m a
+       -> MList m b
+       -> MList m c
+       -> MList m d
+       -> MList m (a,b,c,d)
+zip4ML = zipWith4ML (,,,)
+
+-- |Zips five MLists.
+zip5ML :: Monad m
+       => MList m a
+       -> MList m b
+       -> MList m c
+       -> MList m d
+       -> MList m e
+       -> MList m (a,b,c,d,e)
+zip5ML = zipWith5ML (,,,,)
+
+-- |Zips six MLists.
+zip6ML :: Monad m
+       => MList m a
+       -> MList m b
+       -> MList m c
+       -> MList m d
+       -> MList m e
+       -> MList m f
+       -> MList m (a,b,c,d,e,f)
+zip6ML = zipWith6ML (,,,,,)
+
+-- |Zips seven MLists.
+zip7ML :: Monad m
+       => MList m a
+       -> MList m b
+       -> MList m c
+       -> MList m d
+       -> MList m e
+       -> MList m f
+       -> MList m g
+       -> MList m (a,b,c,d,e,f,g)
+zip7ML = zipWith7ML (,,,,,,)
+
+-- |Zips two lists with a function.
+zipWithML :: Monad m
+        => (a -> b -> c)
+        -> MList m a
+        -> MList m b
+        -> MList m c
+zipWithML f (x :# xs) (y :# ys) =
+  (f x y) :# do xs' <- xs
+                ys' <- ys
+                return (zipWithML f xs' ys')
+zipWithML _ _ _ = MNil
+
+-- |Zips three lists with a function.
+zipWith3ML :: Monad m
+        => (a -> b -> c -> d)
+        -> MList m a
+        -> MList m b
+        -> MList m c
+        -> MList m d
+zipWith3ML f (x :# xs) (y :# ys) (z :# zs) =
+  (f x y z) :# do xs' <- xs
+                  ys' <- ys
+                  zs' <- zs
+                  return (zipWith3ML f xs' ys' zs')
+zipWith3ML _ _ _ _ = MNil
+
+-- |Zips four lists with a function.
+zipWith4ML :: Monad m
+        => (a -> b -> c -> d -> e)
+        -> MList m a
+        -> MList m b
+        -> MList m c
+        -> MList m d
+        -> MList m e
+zipWith4ML f (x :# xs) (y :# ys) (z :# zs) (u :# us) =
+  (f x y z u) :# do xs' <- xs
+                    ys' <- ys
+                    zs' <- zs
+                    us' <- us
+                    return (zipWith4ML f xs' ys' zs' us')
+zipWith4ML _ _ _ _ _ = MNil
+
+-- |Zips five lists with a function.
+zipWith5ML :: Monad m
+        => (a -> b -> c -> d -> e -> f)
+        -> MList m a
+        -> MList m b
+        -> MList m c
+        -> MList m d
+        -> MList m e
+        -> MList m f
+zipWith5ML f (x :# xs) (y :# ys) (z :# zs) (u :# us) (v :# vs) =
+  (f x y z u v) :# do xs' <- xs
+                      ys' <- ys
+                      zs' <- zs
+                      us' <- us
+                      vs' <- vs
+                      return (zipWith5ML f xs' ys' zs' us' vs')
+zipWith5ML _ _ _ _ _ _ = MNil
+
+-- |Zips six lists with a function.
+zipWith6ML :: Monad m
+        => (a -> b -> c -> d -> e -> f -> g)
+        -> MList m a
+        -> MList m b
+        -> MList m c
+        -> MList m d
+        -> MList m e
+        -> MList m f
+        -> MList m g
+zipWith6ML f (x :# xs) (y :# ys) (z :# zs) (u :# us) (v :# vs) (w :# ws) =
+  (f x y z u v w) :# do xs' <- xs
+                        ys' <- ys
+                        zs' <- zs
+                        us' <- us
+                        vs' <- vs
+                        ws' <- ws
+                        return (zipWith6ML f xs' ys' zs' us' vs' ws')
+zipWith6ML _ _ _ _ _ _ _ = MNil
+
+-- |Zips seven lists with a function.
+zipWith7ML :: Monad m
+        => (a -> b -> c -> d -> e -> f -> g -> h)
+        -> MList m a
+        -> MList m b
+        -> MList m c
+        -> MList m d
+        -> MList m e
+        -> MList m f
+        -> MList m g
+        -> MList m h
+zipWith7ML f (x :# xs) (y :# ys) (z :# zs) (u :# us) (v :# vs) (w :# ws) (a :# as) =
+  (f x y z u v w a) :# do xs' <- xs
+                          ys' <- ys
+                          zs' <- zs
+                          us' <- us
+                          vs' <- vs
+                          ws' <- ws
+                          as' <- as
+                          return (zipWith7ML f xs' ys' zs' us' vs' ws' as')
+zipWith7ML _ _ _ _ _ _ _ _ = MNil
