83e19f911d5fcd45feecb80a59af943a7b7dbcbb
Janos Tapolczai <janos.tapolczai@gmail.com>
2015-07-09 02:54:01 +0200

    Added zip[2-8]ML, zipWith[2-8]ML.

--- Control/Monad/Data/List.hs
+++ Control/Monad/Data/List.hs
@@ -98,22 +98,192 @@ elemML x = nullML . dropWhileML (x ==)
 notElemML :: (Monad m, Eq a) => a -> MList m a -> m Bool
 notElemML x xs = not <$> elemML x xs
 
-zipML :: MList m a
+zipWithML :: Monad m
+          => (a -> b -> m c)
+          -> MList m a
+          -> MList m b
+          -> MList m c
+zipWithML f (ML l1) (ML l2) = ML $ do
+   l1' <- l1
+   l2' <- l2
+   case (l1', l2') of
+      (x :# xs, y :# ys) -> (:# zipWithML f xs ys) <$> f x y
+      _ -> return Nil
+
+zipWith3ML :: Monad m
+           => (a -> b -> c -> m d)
+           -> MList m a
+           -> MList m b
+           -> MList m c
+           -> MList m d
+zipWith3ML f (ML l1) (ML l2) (ML l3) = ML $ do
+   l1' <- l1
+   l2' <- l2
+   l3' <- l3
+   case (l1', l2', l3') of
+      (x :# xs, y :# ys, z :# zs) -> (:# zipWith3ML f xs ys zs) <$> f x y z
+      _ -> return Nil
+
+zipWith4ML :: Monad m
+           => (a -> b -> c -> d -> m e)
+           -> MList m a
+           -> MList m b
+           -> MList m c
+           -> MList m d
+           -> MList m e
+zipWith4ML f (ML l1) (ML l2) (ML l3) (ML l4) = ML $ do
+   l1' <- l1
+   l2' <- l2
+   l3' <- l3
+   l4' <- l4
+   case (l1', l2', l3', l4') of
+      (x :# xs, y :# ys, z :# zs, u :# us) -> (:# zipWith4ML f xs ys zs us) <$> f x y z u
+      _ -> return Nil
+
+zipWith5ML :: Monad m
+           => (a -> b -> c -> d -> e -> m f)
+           -> MList m a
+           -> MList m b
+           -> MList m c
+           -> MList m d
+           -> MList m e
+           -> MList m f
+zipWith5ML f (ML l1) (ML l2) (ML l3) (ML l4) (ML l5) = ML $ do
+   l1' <- l1
+   l2' <- l2
+   l3' <- l3
+   l4' <- l4
+   l5' <- l5
+   case (l1', l2', l3', l4', l5') of
+      (x :# xs, y :# ys, z :# zs, u :# us, v :# vs) -> (:# zipWith5ML f xs ys zs us vs) <$> f x y z u v
+      _ -> return Nil
+
+zipWith6ML :: Monad m
+           => (a -> b -> c -> d -> e -> f -> m g)
+           -> MList m a
+           -> MList m b
+           -> MList m c
+           -> MList m d
+           -> MList m e
+           -> MList m f
+           -> MList m g
+zipWith6ML f (ML l1) (ML l2) (ML l3) (ML l4) (ML l5) (ML l6) = ML $ do
+   l1' <- l1
+   l2' <- l2
+   l3' <- l3
+   l4' <- l4
+   l5' <- l5
+   l6' <- l6
+   case (l1', l2', l3', l4', l5', l6') of
+      (x :# xs, y :# ys, z :# zs, u :# us, v :# vs, w :# ws) -> (:# zipWith6ML f xs ys zs us vs ws) <$> f x y z u v w
+      _ -> return Nil
+
+zipWith7ML :: Monad m
+           => (a -> b -> c -> d -> e -> f -> g -> m h)
+           -> MList m a
+           -> MList m b
+           -> MList m c
+           -> MList m d
+           -> MList m e
+           -> MList m f
+           -> MList m g
+           -> MList m h
+zipWith7ML f (ML l1) (ML l2) (ML l3) (ML l4) (ML l5) (ML l6) (ML l7) = ML $ do
+   l1' <- l1
+   l2' <- l2
+   l3' <- l3
+   l4' <- l4
+   l5' <- l5
+   l6' <- l6
+   l7' <- l7
+   case (l1', l2', l3', l4', l5', l6', l7') of
+      (x :# xs, y :# ys, z :# zs, u :# us, v :# vs, w :# ws, a :# as) -> (:# zipWith7ML f xs ys zs us vs ws as) <$> f x y z u v w a
+      _ -> return Nil
+
+zipWith8ML :: Monad m
+           => (a -> b -> c -> d -> e -> f -> g -> h -> m i)
+           -> MList m a
+           -> MList m b
+           -> MList m c
+           -> MList m d
+           -> MList m e
+           -> MList m f
+           -> MList m g
+           -> MList m h
+           -> MList m i
+zipWith8ML f (ML l1) (ML l2) (ML l3) (ML l4) (ML l5) (ML l6) (ML l7) (ML l8) = ML $ do
+   l1' <- l1
+   l2' <- l2
+   l3' <- l3
+   l4' <- l4
+   l5' <- l5
+   l6' <- l6
+   l7' <- l7
+   l8' <- l8
+   case (l1', l2', l3', l4', l5', l6', l7', l8') of
+      (x :# xs, y :# ys, z :# zs, u :# us, v :# vs, w :# ws, a :# as, b :# bs) -> (:# zipWith8ML f xs ys zs us vs ws as bs) <$> f x y z u v w a b
+      _ -> return Nil
+
+
+zipML :: Monad m
+      => MList m a
       -> MList m b
       -> MList m (a,b)
-zipML = undefined
+zipML = zipWithML (\x y -> return (x,y))
 
-zip3ML :: MList m a
+zip3ML :: Monad m
+       => MList m a
        -> MList m b
        -> MList m c
        -> MList m (a,b,c)
-zip3ML = undefined
+zip3ML = zipWith3ML (\x y z -> return (x,y,z))
 
-zip4ML :: MList m a
+zip4ML :: Monad m
+       => MList m a
        -> MList m b
        -> MList m c
        -> MList m d
        -> MList m (a,b,c,d)
-zip4ML = undefined
+zip4ML = zipWith4ML (\x y z u -> return (x,y,z,u))
 
---- to 8
+zip5ML :: Monad m
+       => MList m a
+       -> MList m b
+       -> MList m c
+       -> MList m d
+       -> MList m e
+       -> MList m (a,b,c,d,e)
+zip5ML = zipWith5ML (\x y z u v -> return (x,y,z,u,v))
+
+zip6ML :: Monad m
+       => MList m a
+       -> MList m b
+       -> MList m c
+       -> MList m d
+       -> MList m e
+       -> MList m f
+       -> MList m (a,b,c,d,e,f)
+zip6ML = zipWith6ML (\x y z u v w -> return (x,y,z,u,v,w))
+
+zip7ML :: Monad m
+       => MList m a
+       -> MList m b
+       -> MList m c
+       -> MList m d
+       -> MList m e
+       -> MList m f
+       -> MList m g
+       -> MList m (a,b,c,d,e,f,g)
+zip7ML = zipWith7ML (\x y z u v w a -> return (x,y,z,u,v,w,a))
+
+zip8ML :: Monad m
+       => MList m a
+       -> MList m b
+       -> MList m c
+       -> MList m d
+       -> MList m e
+       -> MList m f
+       -> MList m g
+       -> MList m h
+       -> MList m (a,b,c,d,e,f,g,h)
+zip8ML = zipWith8ML (\x y z u v w a b -> return (x,y,z,u,v,w,a,b))
